c9cc905d1fe54e1f656b915680e26cf03fd23ac1
thuyetnguyen <thuyetdinh9xhn@gmail.com>
2014-07-04 11:47:32 +0700

    first commit

---
+++ asg01.java
@@ -0,0 +1,27 @@
+
+public class asg01 {
+	public static void main(String [] args){
+		System.out.println("ViTatThang\t11020304\tK56CC\tthangvitat\tatthang1106@gmail.com");
+		for(int i=1; i<=20;i++){
+			System.out.println("9 bottles of beer on the wall, 9 bottles of beer.\n"
+							+ "	Take one down, pass it around,\n"
+							+ "	8 bottles of beer on the wall, 8 bottles of beer.\n"
+							+ "	Take one down, pass it around,\n"
+							+ "	7 bottles of beer on the wall, 7 bottles of beer.\n"
+							+ "	Take one down, pass it around,\n"
+							+ "	6 bottles of beer on the wall, 6 bottles of beer.\n"
+							+ "	Take one down, pass it around,\n"
+							+ "	5 bottles of beer on the wall, 5 bottles of beer.\n"
+							+ "	Take one down, pass it around,\n"
+							+ "	4 bottles of beer on the wall, 4 bottles of beer.\n"
+							+ "	Take one down, pass it around,\n"
+							+ "	3 bottles of beer on the wall, 3 bottles of beer.\n"
+							+ "	Take one down, pass it around,\n"
+							+ "	2 bottles of beer on the wall, 2 bottles of beer.\n"
+							+ "	Take one down, pass it around,\n"
+							+ " 1 bottles of beer on the wall, 1 bottles of beer.\n"
+							+ "	Take one down, pass it around,\n");
+		}
+	}
+
+}
